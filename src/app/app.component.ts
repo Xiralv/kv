f011a5bf70a29c72bdacaacf2ea93c1b511a90f3
Vlarix <vlarix@Vlarixs-MacBook-Pro.local>
2025-11-12 11:17:46 +0800

    hide keyboard whenever users press enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import { Keyboard } from '@capacitor/keyboard';
+
+document.addEventListener('keydown', async (e) => {
+  if (e.key === 'Enter') {
+    const active = document.activeElement as HTMLElement;
+    active?.blur();
+    await Keyboard.hide();
+  }
+});
 
 register();
 @Component({
